b953bb9746eeb4548314e12e23c99e8664fea51e
vintech05 <kelanjutannyakelvin@gmail.com>
2024-03-27 21:02:50 +0700

    slowed down the transition duration

--- src/script.js
+++ src/script.js
@@ -1,48 +1,76 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const overlay = document.querySelector('.overlay');
-    const brandLogo = document.querySelector('.brand-logo');
-    const closeBtn = document.querySelector('.close-btn');
-    const navLinks = document.querySelector('.nav-links');
-    const hamburger = document.querySelector('.hamburger');
+document.addEventListener("DOMContentLoaded", function () {
+  const overlay = document.querySelector(".overlay");
+  const brandLogo = document.querySelector(".brand-logo");
+  const closeBtn = document.querySelector(".close-btn");
+  const navLinks = document.querySelector(".nav-links");
+  const hamburger = document.querySelector(".hamburger");
 
-    function toggleTranslate() {
-        brandLogo.classList.toggle('-translate-y-[50em]');
-        closeBtn.classList.toggle('-translate-y-[50em]');
-        navLinks.classList.toggle('-translate-y-[50em]');
-        overlay.classList.toggle('-translate-y-[100em]');
-    }
+  function toggleTranslate() {
+    brandLogo.classList.toggle("-translate-y-[50em]");
+    closeBtn.classList.toggle("-translate-y-[50em]");
+    navLinks.classList.toggle("-translate-y-[50em]");
+    overlay.classList.toggle("-translate-y-[100em]");
+  }
 
-    function addTransition() {
-        brandLogo.classList.add('transition-transform', 'duration-300', 'ease-in-out');
-        closeBtn.classList.add('transition-transform', 'duration-300', 'ease-in-out');
-        navLinks.classList.add('transition-transform', 'duration-300', 'ease-in-out');
-        overlay.classList.add('transition-opacity', 'duration-300', 'ease-in-out');
-    }
+  function addTransition() {
+    brandLogo.classList.add("transition-transform", "duration-500", "ease-out");
+    closeBtn.classList.add("transition-transform", "duration-500", "ease-out");
+    navLinks.classList.add("transition-transform", "duration-500", "ease-out");
+    overlay.classList.add("transition-opacity", "duration-500", "ease-out");
+  }
 
-    function removeTransition() {
-        brandLogo.classList.remove('transition-transform', 'duration-300', 'ease-in-out');
-        closeBtn.classList.remove('transition-transform', 'duration-300', 'ease-in-out');
-        navLinks.classList.remove('transition-transform', 'duration-300', 'ease-in-out');
-        overlay.classList.remove('transition-opacity', 'duration-300', 'ease-in-out');
-    }
+  function removeTransition() {
+    brandLogo.classList.remove(
+      "transition-transform",
+      "duration-700",
+      "ease-out"
+    );
+    closeBtn.classList.remove(
+      "transition-transform",
+      "duration-700",
+      "ease-out"
+    );
+    navLinks.classList.remove(
+      "transition-transform",
+      "duration-700",
+      "ease-out"
+    );
+    overlay.classList.remove("transition-opacity", "duration-700", "ease-out");
+  }
 
-    hamburger.addEventListener('click', function () {
-        toggleTranslate();
-        addTransition();
+  hamburger.addEventListener("click", function () {
+    toggleTranslate();
+    addTransition();
 
-        brandLogo.addEventListener('transitionend', removeTransition, { once: true });
-        closeBtn.addEventListener('transitionend', removeTransition, { once: true });
-        navLinks.addEventListener('transitionend', removeTransition, { once: true });
-        overlay.addEventListener('transitionend', removeTransition, { once: true });
+    hamburger.style.display = "none";
+
+    brandLogo.addEventListener("transitionend", removeTransition, {
+      once: true,
     });
-
-    closeBtn.addEventListener('click', function () {
-        toggleTranslate();
-        addTransition();
-
-        brandLogo.addEventListener('transitionend', removeTransition, { once: true });
-        closeBtn.addEventListener('transitionend', removeTransition, { once: true });
-        navLinks.addEventListener('transitionend', removeTransition, { once: true });
-        overlay.addEventListener('transitionend', removeTransition, { once: true });
+    closeBtn.addEventListener("transitionend", removeTransition, {
+      once: true,
     });
+    navLinks.addEventListener("transitionend", removeTransition, {
+      once: true,
+    });
+    overlay.addEventListener("transitionend", removeTransition, { once: true });
+  });
+
+  closeBtn.addEventListener("click", function () {
+    toggleTranslate();
+    addTransition();
+
+    hamburger.style.display = "block";
+
+    brandLogo.addEventListener("transitionend", removeTransition, {
+      once: true,
+    });
+    closeBtn.addEventListener("transitionend", removeTransition, {
+      once: true,
+    });
+    navLinks.addEventListener("transitionend", removeTransition, {
+      once: true,
+    });
+    overlay.addEventListener("transitionend", removeTransition, { once: true });
+  });
 });
